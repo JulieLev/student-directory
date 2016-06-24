4ca7abf9d3dd98d0f0860b4ee260aeccf5475433
Julie Leverington <jkgee1@yahoo.co.uk>
2016-06-24 18:16:28 +0100

    Asks for user cohort and checks it.

--- directory.rb
+++ directory.rb
@@ -9,8 +9,24 @@ def input_students
       # while the name is not empty, repeat this code
   while !name.empty? do
     # get the cohort
-    puts "Which cohort does #{name} belong to? Please enter the full month name e.g. November"
-    cohortlabel = gets.chomp.to_sym
+    m = false
+    while m == false
+      puts "Which cohort does #{name} belong to? Please enter the month name."
+      cohortlabel = gets.chomp
+      months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+      #check for typos
+      if months.include?(cohortlabel)
+        m = true
+      #check if empty
+      elsif cohortlabel.empty?
+        cohortlabel = "Unknown"
+        m = true
+      else
+        puts "Please enter a valid month: "
+      end #end if
+    end #end second while
+
+    cohortlabel = cohortlabel.to_sym
 
     # add the student hash to the array
     students << {name: name, cohort: cohortlabel, hobbies:[], country:"Not known", height_m: "Not known"}
@@ -18,7 +34,7 @@ def input_students
     puts "Please enter the name of the next one or hit Return twice to finish."
     # get another name from the user
     name = gets.chomp
-  end
+ end #end first while
   # return the array of students
   students
 end
